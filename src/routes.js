046264dd6456f7bd0adf0be299d49f4ecad5d913
Joaovittow <joaovitorseixasn13@outlook.com>
2024-12-08 14:34:27 -0300

    feat(database): Add database connection and User model implementation

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import { v4 } from 'uuid';
+
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.status(200).json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Joaovittow',
+    email: 'joaozinho@email.com',
+    password_hash: '123456',
+  });
+
+  return res.status(201).json(user);
 });
 
 export default routes;
