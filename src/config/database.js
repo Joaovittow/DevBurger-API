a7e9db9a4576115d76943fe6e4f4be6a3cce4b34
joaovittow <joaovitorseixasn13@outlook.com>
2025-03-26 19:46:12 -0300

    chore(database): Add `package-lock.json` and update database name to `postgres` in `database.js`

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'postgres',
-  database: 'devburger',
+  database: 'postgres',
   define: {
     timestamps: true,
     underscored: true,
